7a6f16b330dc9fe8d3fa7ba5f0e8e6e5688b4d50
Klas Anundby <klas.anundby@gmail.com>
2022-01-31 09:57:12 +0100

    Untrack files in .gitignore

---
+++ src/js/gpe_customLocale.js
@@ -0,0 +1,336 @@
+"use strict";
+
+const gpeCustomLocale = 
+[
+    {
+      "ManagerWidgetTitle": {
+        "en-US": "Team widget"
+      },
+      "topNavigationTitle": {
+        "USR": {
+          "en-US": "My Development",
+          "en-UK": "My Development",
+          "de-DE": "Meine Entwicklung"
+        },
+        "MGR": {
+          "en-US": "My team",
+          "en-UK": "My team",
+          "de-DE": "Meine Mannschaft"
+        },
+        "HRD": {
+          "en-US": "HR Dashboard",
+          "en-UK": "HR Dashboard",
+          "de-DE": "HR-Dashboard"
+        },
+        "ADM": {
+          "en-US": "Admin Dashboard",
+          "en-UK": "Administrator",
+          "de-DE": "Admin-Dashboard"
+        },
+        "REC": {
+          "en-US": "Recruiter Dashboard",
+          "en-UK": "Recruiter Dashboard",
+          "de-DE": "Recruiter-Dashboard"
+        },
+        "INS": {
+          "en-US": "Instructor Dashboard",
+          "en-UK": "Instructor Dashboard",
+          "de-DE": "Lehrer-Dashboard"
+        },
+        "APPROVALS": {
+          "en-US": "Pending Approvals",
+          "en-UK": "Pending Approvals",
+          "de-DE": "Zulassungen"
+        }
+      },
+      "onboarding": {
+        "en-US": {
+          "headertitle": "${csFirstName}, your onboarding journey starts here!",
+          "headertext": "Congratulations, and welcome onboard!<br>We are excited that you are joining us and we have prepared for you some useful information before Day 1. Fasten your seatbelt and let the journey begin!",
+          "videourl": "https://scfiles.csod.com/Baseline/common/welcome_to_cornerstone.mp4",
+          "onbprocess": {
+            "title": "What's next on the onboarding",
+            "resourceTitle": "Further resources and/or activities:",
+            "imgurl": "url(/clientimg/demogpe/welcome/onb_wp_process.png)",
+            "textItem": {
+              "nextstepstext1": {
+                "headline": "First day",
+                "text": "As a new member of team you will initially be introduced to your new colleagues by your manager. Your manager will also help you understand our processes and systems to get started. You can already now start to familiarize yourself by opening up the New Hire Curriculum below.",
+                "resources": {
+                  "res1": {
+                    "text": "Read more about is on our Company website",
+                    "url": "https://www.cornerstoneondemand.com/company/",
+                    "type": "url"
+                  },
+                  "res2": {
+                    "text": "Follow us on LinkedIn",
+                    "url": "https://www.linkedin.com/company/cornerstone-ondemand",
+                    "type": "url"
+                  },
+                  "res3": {
+                    "text": "Fill out form - Personal Contact Details",
+                    "url": "/phnx/driver.aspx?routename=CustomForm/Create_Change_Contact_Details_002_frm_Edit_1&cfqs=%5e%5e%5ecsAJl9tGgheNnOyu8GlE8K6r80OhoI17mYrdxw86mqqyfExbEIrnU5YTbP2Svv8FgSnRkmhgXTrOE2Vd2J4C48aqSGGgD1Cc954k1FiWNcKwYrNZCkBr%2fm18odFjDYs1Z4IAcqcoia8N%2fvWUbFTIUqTaCUKFX4rcGsnSasW%2fyWcbQKxao12LMJ5qYkoW4wAicBqp5JNtrJ4m4etQV10Xaw%3d%3d",
+                    "type": "url"
+                  },
+                  "res4": {
+                    "text": "Update your Bio page in Universal Profile",
+                    "url": "/phnx/driver.aspx?routename=Social/UniversalProfile/Bio",
+                    "type": "url"
+                  }
+                }
+              },
+              "nextstepstext2": {
+                "headline": "First week",
+                "text": "Being new at the job can be stressfull, we want you to clearly understand the expectations of the job as well as the company culture. We believe it will help you and your performance. You manager and/or mentor will support you in getting up to speed.",
+                "resources": {
+                  "res1": {
+                    "text": "Go through your Onboarding Curriculum",
+                    "url": "/DeepLink/ProcessRedirect.aspx?module=loRegisterAndLaunch&lo=0ffd0bdc-e67a-4283-bdb1-79b4ab25189e",
+                    "type": "url"
+                  },
+                  "res2": {
+                    "text": "Be recommended training for your role",
+                    "url": "/ui/lms-learner-home/home",
+                    "type": "url"
+                  },
+                  "res3": {
+                    "text": "Create your own playlist together with your manager",
+                    "url": "/ui/lms-learner-playlist/UsersPlaylists",
+                    "type": "url"
+                  }
+                }
+              },
+              "nextstepstext3": {
+                "headline": "First month",
+                "text": "By spending time together with your manager and/or mentor you will shortly be able to set valuable goals supporting our company vision/strategy. We will also be having a social get together for all new hires where you will be able to connect with the entire team!",
+                "resources": {
+                  "res1": {
+                    "text": "Schedule regular check-in meetings with your manager",
+                    "type": "text"
+                  },
+                  "res2": {
+                    "text": "Create goals",
+                    "type": "text"
+                  },
+                  "res3": {
+                    "text": "Have fun!",
+                    "type": "text"
+                  }
+                }
+              },
+              "nextstepstext4": {
+                "headline": "First 6 months",
+                "text": "When you succeed - we succeed! We're a group. We're a team. From the CEO  and on through, we're a team. We win together, we lose together. We celebrate and we mourn together. And defeats are softened and victories sweeter because we did them together. At the end of the day - staying healthy and motivated is key for us all.",
+                "resources": {
+                  "res1": {
+                    "text": "Create your Personal Development Plan",
+                    "type": "text"
+                  },
+                  "res2": {
+                    "text": "Get Certified",
+                    "type": "text"
+                  },
+                  "res3": {
+                    "text": "Receive and give badges",
+                    "type": "text"
+                  }
+                }
+              }
+            }
+          }
+        },
+        "en-UK": {
+          "headertitle": "${csFirstName}, your onboarding journey starts here!",
+          "headertext": "Congratulations, and welcome onboard!<br>We are excited that you are joining us and we have prepared for you some useful information before Day 1. Fasten your seatbelt and let the journey begin!",
+          "videourl": "https://scfiles.csod.com/Baseline/common/welcome_to_cornerstone.mp4",
+          "onbprocess": {
+            "title": "What's next on the onboarding",
+            "resourceTitle": "Further resources and/or activities:",
+            "imgurl": "url(/clientimg/demogpe/welcome/onb_wp_process.png)",
+            "text": {
+              "nextstepstext1": {
+                "headline": "First day",
+                "text": "As a new member of team you will initially be introduced to your new colleagues by your manager. Your manager will also help you understand our processes and systems to get started. You can already now start to familiarize yourself by opening up the New Hire Curriculum below.",
+                "resources": {
+                  "res1": {
+                    "text": "Read more about is on our Company website",
+                    "url": "https://www.cornerstoneondemand.com/company/",
+                    "type": "url"
+                  },
+                  "res2": {
+                    "text": "Follow us on LinkedIn",
+                    "url": "https://www.linkedin.com/company/cornerstone-ondemand",
+                    "type": "url"
+                  },
+                  "res3": {
+                    "text": "Fill out form - Personal Contact Details",
+                    "url": "/phnx/driver.aspx?routename=CustomForm/Create_Change_Contact_Details_002_frm_Edit_1&cfqs=%5e%5e%5ecsAJl9tGgheNnOyu8GlE8K6r80OhoI17mYrdxw86mqqyfExbEIrnU5YTbP2Svv8FgSnRkmhgXTrOE2Vd2J4C48aqSGGgD1Cc954k1FiWNcKwYrNZCkBr%2fm18odFjDYs1Z4IAcqcoia8N%2fvWUbFTIUqTaCUKFX4rcGsnSasW%2fyWcbQKxao12LMJ5qYkoW4wAicBqp5JNtrJ4m4etQV10Xaw%3d%3d",
+                    "type": "url"
+                  },
+                  "res4": {
+                    "text": "Update your Bio page in Universal Profile",
+                    "url": "/phnx/driver.aspx?routename=Social/UniversalProfile/Bio",
+                    "type": "url"
+                  }
+                }
+              },
+              "nextstepstext2": {
+                "headline": "First week",
+                "text": "Being new at the job can be stressfull, we want you to clearly understand the expectations of the job as well as the company culture. We believe it will help you and your performance. You manager and/or mentor will support you in getting up to speed.",
+                "resources": {
+                  "res1": {
+                    "text": "Go through your Onboarding Curriculum",
+                    "url": "/DeepLink/ProcessRedirect.aspx?module=loRegisterAndLaunch&lo=0ffd0bdc-e67a-4283-bdb1-79b4ab25189e",
+                    "type": "url"
+                  },
+                  "res2": {
+                    "text": "Be recommended training for your role",
+                    "url": "/ui/lms-learner-home/home",
+                    "type": "url"
+                  },
+                  "res3": {
+                    "text": "Create your own playlist together with your manager",
+                    "url": "/ui/lms-learner-playlist/UsersPlaylists",
+                    "type": "url"
+                  }
+                }
+              },
+              "nextstepstext3": {
+                "headline": "First month",
+                "text": "By spending time together with your manager and/or mentor you will shortly be able to set valuable goals supporting our company vision/strategy. We will also be having a social get together for all new hires where you will be able to connect with the entire team!",
+                "resources": {
+                  "res1": {
+                    "text": "Schedule regular check-in meetings with your manager",
+                    "type": "text"
+                  },
+                  "res2": {
+                    "text": "Create goals",
+                    "type": "text"
+                  },
+                  "res3": {
+                    "text": "Have fun!",
+                    "type": "text"
+                  }
+                }
+              },
+              "nextstepstext4": {
+                "headline": "First 6 months",
+                "text": "When you succeed - we succeed! We're a group. We're a team. From the CEO  and on through, we're a team. We win together, we lose together. We celebrate and we mourn together. And defeats are softened and victories sweeter because we did them together. At the end of the day - staying healthy and motivated is key for us all.",
+                "resources": {
+                  "res1": {
+                    "text": "Create your Personal Development Plan",
+                    "type": "text"
+                  },
+                  "res2": {
+                    "text": "Get Certified",
+                    "type": "text"
+                  },
+                  "res3": {
+                    "text": "Receive and give badges",
+                    "type": "text"
+                  }
+                }
+              }
+            }
+          }
+        },
+        "de-DE": {
+          "headertitle": "${csFirstName}, your onboarding journey starts here!",
+          "headertext": "Congratulations, and welcome onboard!<br>We are excited that you are joining us and we have prepared for you some useful information before Day 1. Fasten your seatbelt and let the journey begin!",
+          "videourl": "https://scfiles.csod.com/Baseline/common/welcome_to_cornerstone.mp4",
+          "onbprocess": {
+            "title": "What's next on the onboarding",
+            "resourceTitle": "Further resources and/or activities:",
+            "imgurl": "url(/clientimg/demogpe/welcome/onb_wp_process.png)",
+            "text": {
+              "nextstepstext1": {
+                "headline": "First day",
+                "text": "As a new member of team you will initially be introduced to your new colleagues by your manager. Your manager will also help you understand our processes and systems to get started. You can already now start to familiarize yourself by opening up the New Hire Curriculum below.",
+                "resources": {
+                  "res1": {
+                    "text": "Read more about is on our Company website",
+                    "url": "https://www.cornerstoneondemand.com/company/",
+                    "type": "url"
+                  },
+                  "res2": {
+                    "text": "Follow us on LinkedIn",
+                    "url": "https://www.linkedin.com/company/cornerstone-ondemand",
+                    "type": "url"
+                  },
+                  "res3": {
+                    "text": "Fill out form - Personal Contact Details",
+                    "url": "/phnx/driver.aspx?routename=CustomForm/Create_Change_Contact_Details_002_frm_Edit_1&cfqs=%5e%5e%5ecsAJl9tGgheNnOyu8GlE8K6r80OhoI17mYrdxw86mqqyfExbEIrnU5YTbP2Svv8FgSnRkmhgXTrOE2Vd2J4C48aqSGGgD1Cc954k1FiWNcKwYrNZCkBr%2fm18odFjDYs1Z4IAcqcoia8N%2fvWUbFTIUqTaCUKFX4rcGsnSasW%2fyWcbQKxao12LMJ5qYkoW4wAicBqp5JNtrJ4m4etQV10Xaw%3d%3d",
+                    "type": "url"
+                  },
+                  "res4": {
+                    "text": "Update your Bio page in Universal Profile",
+                    "url": "/phnx/driver.aspx?routename=Social/UniversalProfile/Bio",
+                    "type": "url"
+                  }
+                }
+              },
+              "nextstepstext2": {
+                "headline": "First week",
+                "text": "Being new at the job can be stressfull, we want you to clearly understand the expectations of the job as well as the company culture. We believe it will help you and your performance. You manager and/or mentor will support you in getting up to speed.",
+                "resources": {
+                  "res1": {
+                    "text": "Go through your Onboarding Curriculum",
+                    "url": "/DeepLink/ProcessRedirect.aspx?module=loRegisterAndLaunch&lo=0ffd0bdc-e67a-4283-bdb1-79b4ab25189e",
+                    "type": "url"
+                  },
+                  "res2": {
+                    "text": "Be recommended training for your role",
+                    "url": "/ui/lms-learner-home/home",
+                    "type": "url"
+                  },
+                  "res3": {
+                    "text": "Create your own playlist together with your manager",
+                    "url": "/ui/lms-learner-playlist/UsersPlaylists",
+                    "type": "url"
+                  }
+                }
+              },
+              "nextstepstext3": {
+                "headline": "First month",
+                "text": "By spending time together with your manager and/or mentor you will shortly be able to set valuable goals supporting our company vision/strategy. We will also be having a social get together for all new hires where you will be able to connect with the entire team!",
+                "resources": {
+                  "res1": {
+                    "text": "Schedule regular check-in meetings with your manager",
+                    "type": "text"
+                  },
+                  "res2": {
+                    "text": "Create goals",
+                    "type": "text"
+                  },
+                  "res3": {
+                    "text": "Have fun!",
+                    "type": "text"
+                  }
+                }
+              },
+              "nextstepstext4": {
+                "headline": "First 6 months",
+                "text": "When you succeed - we succeed! We're a group. We're a team. From the CEO  and on through, we're a team. We win together, we lose together. We celebrate and we mourn together. And defeats are softened and victories sweeter because we did them together. At the end of the day - staying healthy and motivated is key for us all.",
+                "resources": {
+                  "res1": {
+                    "text": "Create your Personal Development Plan",
+                    "type": "text"
+                  },
+                  "res2": {
+                    "text": "Get Certified",
+                    "type": "text"
+                  },
+                  "res3": {
+                    "text": "Receive and give badges",
+                    "type": "text"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  ];
+
+  sessionStorage.setItem("csCustomLocale", JSON.stringify(gpeCustomLocale));
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyPlugin = require("copy-webpack-plugin");
+
+var PACKAGE = require('./package.json');
+var version = PACKAGE.version;
+
+module.exports = {
+    mode: 'production',
+    entry: ["regenerator-runtime/runtime.js", "./src/gpeWelcomePage.js"], 
+    output: {
+        path: path.resolve(__dirname, 'build', version),
+        filename: "gpeWelcomePage.min.js",
+        environment: {
+            arrowFunction: false,
+            bigIntLiteral: false,
+            const: false,
+            destructuring: false,
+            dynamicImport: false,
+            forOf: false,
+            module: false,
+          },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    "assumptions": {
+                        "setPublicClassFields": true
+                    },
+                    plugins: ['@babel/plugin-proposal-object-rest-spread',
+                        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+                        "@babel/plugin-proposal-class-properties",
+                        ["@babel/plugin-transform-parameters"],
+                        ["@babel/plugin-transform-property-mutators"],
+                        ["@babel/plugin-transform-shorthand-properties"],
+                        ["@babel/plugin-transform-arrow-functions", { "spec": true }],
+                        ["@babel/plugin-transform-runtime",
+                            {
+                              "absoluteRuntime": true,
+                              "corejs": false,
+                              "helpers": false,
+                              "regenerator": false,
+                              "version": "7.0.0-beta.0"
+                            }
+                        ]
+                    ]
+                }
+            },
+        ],
+    },
+    plugins: [    
+        new HtmlWebpackPlugin({
+          template: './src/gpeWelcomePage.html',
+          filename: "gpeWelcomePage.min.html",
+          inject: false
+        }),
+        new CopyPlugin({
+            patterns: [
+              {
+                from: "src/js/gpe_widgetConfig.min.js",
+                to: "gpe_widgetConfig-min.js",
+              },
+              {
+                from: "src/js/gpe_customLocale.min.js",
+                to: "gpe_customLocale-min.js",
+              }
+            ]
+        })
+      ],        
+    stats: {
+        colors: true,
+        env: true,
+        ids: true
+
+    }
+    // devtool: 'source-map'
+};
